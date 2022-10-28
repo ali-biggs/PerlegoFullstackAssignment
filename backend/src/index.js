7071f6d0cfcf61ee80685796f60f13dbc08e7f99
ali-biggs <aliya.f.biggs@gmail.com>
2022-10-28 14:05:10 +0100

    installed mysql

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,32 @@
-const express = require('express')
+const express = require('express');
+const mysql = require('mysql');
 
-const app = express()
+const app = express();
 const port = 3000
 
+const connection = mysql.createConnection({
+    host: 'localhost:8001',
+    user: 'perlego',
+    password: 'perlego',
+    database: 'perlego'
+})
+
+connection.connect()
+
 app.get('/', (req, res) => {
-    res.json({message: 'Hello, friend!'})
+    res.json({ message: 'Hello, friend!' })
 })
 
 app.get('/books/suggestions', (req, res) => {
     res.json({
         count: 1,
-        books:[
+        books: [
             {
                 id: 1,
                 title: 'foo',
                 author: 'bar',
-                cover: 'https://www.perlego.com/books/RM_Books/baker_academic_gcmneit/9781493405961_300_450.jpg'},
+                cover: 'https://www.perlego.com/books/RM_Books/baker_academic_gcmneit/9781493405961_300_450.jpg'
+            },
         ]
     })
 })
