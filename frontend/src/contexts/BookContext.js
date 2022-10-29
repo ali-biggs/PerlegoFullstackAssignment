568baac275f8a641c90c0ae3deaed78cf2e3cfdc
ali-biggs <aliya.f.biggs@gmail.com>
2022-10-29 20:16:10 +0100

    4 colums to allow for reading list

---
+++ frontend/src/contexts/BookContext.js
@@ -0,0 +1,23 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+    const [books, setBooks] = useState([]);
+
+    const addBook = (title, author) => {
+        setBooks([...books, { title, author, id }]);
+    };
+
+    const removeBook = (id) => {
+        setBooks(books.filter(book => book.id !== id));
+    };
+
+    return (
+        <BookContext.Provider value={{ books, addBook, removeBook }}>
+            {props.children}
+        </BookContext.Provider>
+    )
+}
+
+export default BookContextProvider;
